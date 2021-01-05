f9dcea587695ef8ad6bbfba08a1b38ca786b616b
Alina Simionescu <alinasimionescu505@yahoo.com>
2021-01-05 10:49:18 +0200

    Add project files.

---
+++ Simionescu_Alina_Lab12/Simionescu_Alina_Lab12/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Simionescu_Alina_Lab12.Data;
+
+
+namespace Simionescu_Alina_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+          
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Simionescu_Alina_Lab12/Simionescu_Alina_Lab12/Data/IRestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Simionescu_Alina_Lab12.Models;
+
+namespace Simionescu_Alina_Lab12.Data
+{
+  
+        public interface IRestService
+        {
+            Task<List<ShopList>> RefreshDataAsync();
+            Task SaveShopListAsync(ShopList item, bool isNewItem);
+            Task DeleteShopListAsync(int id);
+        }
+    }
+
